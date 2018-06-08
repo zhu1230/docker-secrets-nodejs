9d3ee56b68d6f35adc2c137813beea8b6e062dcd
Vincent Zhu <zhu1230@gmail.com>
2018-06-08 17:49:13 +0100

    update dist file

--- dist/index.js
+++ dist/index.js
@@ -8,7 +8,10 @@ const init = () => {
     if (fs.existsSync(SECRETS_DIR)) {
         const files = fs.readdirSync(SECRETS_DIR);
         config = files.reduce((resultObject, file) => {
-            resultObject[file] = fs.readFileSync(path.join(SECRETS_DIR, file), 'utf8').toString().trim();
+            let file_path = path.join(SECRETS_DIR, file);
+            if (fs.statSync(file_path).isFile()) {
+                resultObject[file] = fs.readFileSync(file_path, 'utf8').toString().trim();
+            }
             return resultObject;
         }, {});
     }
